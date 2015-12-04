e65f33d35803612cfbe9dc640a812259d11ef866
bararchy <bar.hofesh@safe-t.com>
2015-12-04 18:24:58 +0200

    Need to understand how to pass the data into the NN (WordBag?)

--- ann.rb
+++ ann.rb
@@ -400,17 +400,91 @@ module Scaler
 end
 
 
-bag = BagOfWords.new idf: true
-File.open("testing.txt").each do |line|
-  doc = URI.decode(line.chomp.to_s)
-  bag.add_doc doc
-end
+# bag = BagOfWords.new idf: true
+# File.open("testing.txt").each do |line|
+#   doc = URI.decode(line.chomp.to_s)
+#   bag.add_doc doc
+# end
 
-data = bag.to_matrix
-puts "BAG:"
-puts bag.inspect
-puts "Matrix:"
-puts data.inspect
-data = Scaler.scale data
-puts "Scaled"
-puts data.inspect
+# data = bag.to_matrix
+# puts "BAG:"
+# puts bag.inspect
+# puts "Matrix:"
+# puts data.inspect
+# data = Scaler.scale data
+# puts "Scaled"
+# puts data.inspect
+
+
+rows = File.readlines("testing.txt").map {|l| l.chomp.split(',') }
+
+rows.shuffle!
+
+label_encodings = {
+  "Normal"     => [1, 0, 0],
+  "SQLi" => [0, 1, 0],
+  "XSS"  => [0, 0 ,1]
+}
+
+x_data = rows.map {|row| row[0,4].map }
+y_data = rows.map {|row| label_encodings[row[2]] }
+
+
+x_train = x_data.slice(0, 100)
+y_train = y_data.slice(0, 100)
+
+x_test = x_data.slice(100, 50)
+y_test = y_data.slice(100, 50)
+
+
+nn = NeuralNet.new [4,4,3]
+
+prediction_success = -> (actual, ideal) {
+  predicted = (0..2).max_by {|i| actual[i] }
+  ideal[predicted] == 1 
+}
+
+mse = -> (actual, ideal) {
+  errors = actual.zip(ideal).map {|a, i| a - i }
+  (errors.inject(0) {|sum, err| sum += err**2}) / errors.length.to_f
+}
+
+error_rate = -> (errors, total) { ((errors / total.to_f) * 100).round }
+
+run_test = -> (nn, inputs, expected_outputs) {
+  success, failure, errsum = 0,0,0
+  inputs.each.with_index do |input, i|
+    output = nn.run input
+    prediction_success.(output, expected_outputs[i]) ? success += 1 : failure += 1
+    errsum += mse.(output, expected_outputs[i])
+  end
+  [success, failure, errsum / inputs.length.to_f]
+}
+
+puts "Testing the untrained network..."
+
+success, failure, avg_mse = run_test.(nn, x_test, y_test)
+
+puts "Untrained classification success: #{success}, failure: #{failure} (classification error: #{error_rate.(failure, x_test.length)}%, mse: #{(avg_mse * 100).round(2)}%)"
+
+
+puts "\nTraining the network...\n\n"
+
+t1 = Time.now
+result = nn.train(x_train, y_train, error_threshold: 0.01, 
+                                    max_iterations: 1_000,
+                                    log_every: 100
+                                    )
+
+# puts result
+puts "\nDone training the network: #{result[:iterations]} iterations, #{(result[:error] * 100).round(2)}% mse, #{(Time.now - t1).round(1)}s"
+
+
+puts "\nTesting the trained network..."
+
+success, failure, avg_mse = run_test.(nn, x_test, y_test)
+
+puts "Trained classification success: #{success}, failure: #{failure} (classification error: #{error_rate.(failure, x_test.length)}%, mse: #{(avg_mse * 100).round(2)}%)"
+
+    Status API Training Shop Blog About Pricing 
+
