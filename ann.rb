45f20200406f53552deb87fc144fdf6521e835a6
bararchy <bar.hofesh@safe-t.com>
2015-12-07 18:12:09 +0200

    Added save state

--- ann.rb
+++ ann.rb
@@ -22,8 +22,13 @@ File.open("data/normal_http_traffic.txt").each do |line|
 end
 
 # Initialize the Neural Net, [input neurons, hidden neurons, output neurons]
-nn = NeuralNet.new [bag.terms_count, 50, 2]
-
+if File.exist?("/tmp/ann.db")
+  puts "Loading saved NN from file".bold.green
+  nn = Marshal.load(File.read("/tmp/ann.db"))
+else
+  puts "Initializing a new NN instance".bold.green
+  nn = NeuralNet.new [bag.terms_count, 50, 2]
+end
 # Load all the files again to create the y_data object (labels and line numbers)
 rows = File.readlines("data/abnormal_http_traffic.txt").map {|l| l.chomp.split(',') }
 rows = rows + File.readlines("data/normal_http_traffic.txt").map {|l| l.chomp.split(',') }
@@ -33,8 +38,8 @@ rows.shuffle!
 
 # Set labels to understand the output options
 label_encodings = {
-  "Normal"     => [1, 0, 0],
-  "Abnormal" => [0, 1, 0]
+  "Normal"     => [1,0],
+  "Abnormal" => [0,1]
 }
 
 x_data = bag.to_a
@@ -86,7 +91,7 @@ puts ["Untrained classification success: ","#{success},".green, "failure: ","#{f
 puts "\nTraining the network...\n\n".bold
 
 t1 = Time.now
-result = nn.train(x_train, y_train, error_threshold: 0.01, max_iterations: 1_000, log_every: 1)
+result = nn.train(x_train, y_train, error_threshold: 0.01, max_iterations: 60, log_every: 10)
 
 # puts result
 puts "\nDone training the network: #{result[:iterations]} iterations, #{(result[:error] * 100).round(2)}% mse, #{(Time.now - t1).round(1)}s"
@@ -96,17 +101,18 @@ puts "\nTesting the trained network..."
 
 success, failure, avg_mse = run_test.(nn, x_test, y_test)
 
-puts "Trained classification success: #{success}, failure: #{failure} (classification error: #{error_rate.(failure, x_test.length)}%, mse: #{(avg_mse * 100).round(2)}%)"
+puts ["Trained classification success: ","#{success},".green, "failure: ","#{failure}".red, "(classification error: #{error_rate.(failure, x_test.length)}%, mse: #{(avg_mse * 100).round(2)}%)"].join
 
 puts "Testing Single input"
 
 new_bag = BagOfWords.new idf: true
-new_bag.add_doc "index.html?id=12'OR 'a'='a' SELECT FROM DATA"
-
+new_bag.add_doc URI.decode("GET /Safe-T/Webskin/DefaultWebskin/Tools/bootstrap-3.1.1/css/bootstrap.min.css HTTP/1.1Host: %3B %20 SELECT %20 SLEEP %28 5 %29 &#45;- %20User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0Accept: text/css*/*;q=0.1Accept-Language: en-USen;q=0.5Accept-Encoding: gzip deflateDNT: 1Referer: https://192.168.4.109/Safe-T/Webskin/DefaultWebskin/Style/Main.cssCookie: clz02j6mgdqpzoey8i5010pz; SFTSId=dnsm0flp3iimy0oxjttymdys; .safeApiAuth=6AF734B51DC559EAB8F898DAB445D97910657850CFE6B9A2FEDC71F3468A4D3C51431F27F051215A2A743D6E854DC58AA0E0BE984E0A964A6802D81103FBAE4341BF760022F8309B8E7929C983B1B1D861D13572293091CB062ECAFB35F3CBF7ABBCC258C1031923DDA0ACD390CE0BB185E7F892; __tempRemoteIdentifier__=409a1cb3549f4f8b9f13430035208736Connection: keep-alive")
+ytrain = [[1,0]]
 xtrain = new_bag.to_a.slice(0,1)
-puts xtrain.inspect
+success, failure, avg_mse = run_test.(nn, xtrain, ytrain)
+puts "Success: #{success}, Failure: #{failure}"
 
-output = nn.run xtrain[0]
-puts output.inspect
-predicted = (0..1).max_by {|i| output[i] }
-puts predicted
+puts "Saving State at /tmp/ann.db"
+f = File.open("/tmp/ann.db", 'w')
+f.write(Marshal.dump(nn))
+f.close
